0600677ae64238e024d31cc0464b93c3db0f2535
lauris79 <latpages@gmail.com>
2021-08-17 09:18:49 +0300

    AdminController user create fix - default profile type 0

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -317,6 +317,11 @@ class AdminController extends Controller
 			$model->attributes=$post_user;
 			$model->activkey=Yii::app()->controller->module->encrypting(microtime().$model->password);
 			$profile->attributes=$_POST['Profile'];
+			
+			if (!$profile->type) {
+                $profile->type = 0;
+            }
+			
 			$profile->user_id=0;
 			if($model->validate()&&$profile->validate()) {
 				$model->password=Yii::app()->controller->module->encrypting($model->password);
